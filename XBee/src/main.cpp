b619aa8bf82880af45b0784dae760a75c4d1ffc8
OtarieSupreme <gaby.mouchette@gmail.com>
2024-06-20 11:53:50 +0200

    Màj main Xbee

--- XBee/src/main.cpp
+++ XBee/src/main.cpp
@@ -20,13 +20,15 @@ int main() {
     //démarer l'écoute pour reception de trame (car sur processus séparé)
     xbee.startListening();
 
+    while (true) {
     //demande d'information à une caméra présise avec fonction de demande des positions, aucune donnée n'est envoyé, et un temps d'attente d'info max de 5 seconde (s'arrete si aucune réponse)
     xbee_result_t res = xbee.send(XB_ADDR_CAMERA_01, XB_FCT_GET_ARUCO_POS, {}, 5);
 
     //arret du programme en cas d'erreur
     if (res.status != XB_E_SUCCESS) {
         std::cout << "Erreur de communication" << std::endl;
-        return 1;
+        //return 1;
+        continue;
     }
 
     //raccourcis vers les données envoyés récupérés
@@ -43,13 +45,44 @@ int main() {
     // => [Id_code, position_X_code_/_code_ref, position_Y_..., position_Z_..., orientation_axe_Z_...]
     for (int i=1; i < data.size(); i+=9) {
         uint16_t IDc = data[i];
-        uint16_t Xr = (data[i+1] << 8) | data[i+2];
-        uint16_t Yr = (data[i+3] << 8) | data[i+4];
-        uint16_t Zr = (data[i+5] << 8) | data[i+6];
-        uint16_t TZr = (data[i+7] << 8) | data[i+8];
+        uint16_t X = (data[i+1] << 8) | data[i+2];
+        uint16_t Y = (data[i+3] << 8) | data[i+4];
+        uint16_t Z = (data[i+5] << 8) | data[i+6];
+        uint16_t TZ = (data[i+7] << 8) | data[i+8];
         //push_back = ajouter à la fin les varibales (créé une nouvelle ligne)
-        markers_pos.push_back({IDc, Xr, Yr, Zr, TZr});
-        std::cout << std::dec << IDc << " " << Xr << " " << Yr << " " << Zr << " " << TZr << std::endl;
+        markers_pos.push_back({IDc, X, Y, Z, TZ});
+        std::cout << std::dec << IDc << " : x= " << X << ", y= " << Y << ", z= " << Z << ", Tz= " << TZ << std::endl;
+    }
+
+    std::array<uint16_t, 5> position_robot;
+    int markerRobot = 53;
+    int numeroCamera = 1;
+    bool empty = 0;
+    int pasTrouve = 0;
+
+    for (int i=0; i < markers_pos.size(); i++){
+        if (markers_pos[i][0] == markerRobot) {
+            pasTrouve = -markers_pos.size()+1;
+            empty = 0;
+            uint16_t Xr = markers_pos[i][1];
+            uint16_t Yr = markers_pos[i][2];
+            uint16_t TZr = markers_pos[i][4];
+            uint16_t numCam = numeroCamera;
+            position_robot = {empty, Xr, Yr, TZr, numCam};
+            }
+        else pasTrouve++;
+    }
+
+    if (pasTrouve >= 0 ) {
+        uint16_t Xr = 0;
+        uint16_t Yr = 0;
+        uint16_t TZr = 0;
+        uint16_t numCam = numeroCamera;
+        position_robot = {empty, Xr, Yr, TZr, numCam};
+        std::cout << "Le Robot n'a pas été repéré" << std::endl;
+        }
+    else std::cout << "Le Robot a  été repéré aux coords : Xr= " << position_robot[1] << ", Yr= " << position_robot[2] << ", et orientation TZr= " << position_robot[3] << std::endl;
+
     }
 
     return XB_E_SUCCESS;
